4915a6796f0a24422dee642f910018f91d312c07
Ron Ferguson <r0nn1ef8580@gmail.com>
2019-10-17 16:45:35 -0500

    added missing API method wrappers

--- src/Service/GoogleYoutubeClient.php
+++ src/Service/GoogleYoutubeClient.php
@@ -68,6 +68,98 @@ class GoogleYoutubeClient {
   }
 
   /**
+   * Wrapper for \Google_Service_YouTube_Resource_Videos::listVideos().
+   *
+   * @param mixed array|strinng $parts
+   * @param $optParams
+   *
+   * @return mixed
+   */
+  public function listVideos($parts, $optParams) {
+    if (is_array($parts)) {
+      $parts = implode(',', $parts);
+    }
+    return $this->googleServiceYouTube->videos->listVideos($parts, $optParams);
+  }
+
+  /**
+   * Wrapper for \Google_Service_YouTube_Resource_Videos::update().
+   *
+   * @param mixed array|string $parts
+   * @param \Google_Service_YouTube_Video $video
+   *
+   * @return mixed
+   */
+  public function updateVideo($parts, \Google_Service_YouTube_Video $video) {
+    if (is_array($parts)) {
+      $parts = implode(',', $parts);
+    }
+    return $this->googleServiceYouTube->videos->update($parts, $video);
+  }
+
+  /**
+   * Wrapper for \Google_Service_YouTube_Resource_Videos::delete().
+   *
+   * @param string $id
+   * @param array $optParams
+   *
+   * @return mixed
+   */
+  public function delete($id, $optParams = []) {
+    return $this->googleServiceYouTube->videos->delete($id, $optParams);
+  }
+
+  /**
+   * Wrapper for \Google_Service_YouTube_Resource_Videos::insert().
+   *
+   * @param $part
+   * @param \Google_Service_YouTube_Video $postBody
+   * @param array $optParams
+   *
+   * @return mixed
+   */
+  public function insert($part, \Google_Service_YouTube_Video $postBody, $optParams = []) {
+    return $this->googleServiceYouTube->videos->insert($part, $postBody, $optParams);
+  }
+
+  /**
+   * Wrapper for \Google_Service_YouTube_Resource_Videos::rate().
+   *
+   * @param $id
+   * @param $rating
+   * @param array $optParams
+   *
+   * @return mixed
+   */
+  public function rate($id, $rating, $optParams = []) {
+    return $this->googleServiceYouTube->videos->rate($id, $rating, $optParams);
+  }
+
+  /**
+   * Wrapper for \Google_Service_YouTube_Resource_Videos::getRating().
+   *
+   * @param $id
+   * @param array $optParams
+   *
+   * @return mixed
+   */
+  public function getRating($id, $optParams = []) {
+    return $this->googleServiceYouTube->videos->getRating($id, $optParams);
+  }
+
+  /**
+   * Wrapper for \Google_Service_YouTube_Resource_Videos::reportAbuse().
+   *
+   * @param \Google_Service_YouTube_VideoAbuseReport $postBody
+   * @param array $optParams
+   *
+   * @return mixed
+   */
+  public function reportAbuse(\Google_Service_YouTube_VideoAbuseReport $postBody, $optParams = []) {
+    return $this->googleServiceYouTube->videos->reportAbuse($postBody, $optParams);
+  }
+
+  /**
    * Helper method to getGoogleYoutubeClient.
    *
    * @return \Google_Service_YouTube
@@ -78,6 +170,7 @@ class GoogleYoutubeClient {
       "https://www.googleapis.com/auth/youtube",
       "https://www.googleapis.com/auth/youtubepartner",
       "https://www.googleapis.com/auth/youtube.upload",
+      "https://www.googleapis.com/auth/youtube.force-ssl",
     ]);
 
     // Set up the YouTube Client that interacts with the API.
